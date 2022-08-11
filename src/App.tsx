c6a0915c5860ccc9d4534e0ce6bb890a8040e773
Prizzz <rob_243248@mail.ru>
2022-08-11 19:50:16 +0400

    reafactor: add custom hook for products

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
-import axios, { Axios, AxiosError } from "axios";
-import React, { useEffect, useState } from "react";
 import Product from "./components/Product";
-import { IProduct } from "./models";
+import { useProducts } from "./components/hooks/products";
 
 function App() {
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  async function fetchProducts() {
-    try {
-      setError("");
-      setLoading(true);
-      const res = await axios.get<IProduct[]>(
-        "https://fakestoreapi.com/products?limit=8"
-      );
-      setProducts(res.data);
-      setLoading(false);
-    } catch (e: unknown) {
-      const error = e as AxiosError;
-      setLoading(false);
-      setError(error.message);
-    }
-  }
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
+  const { loading, products, error } = useProducts();
   return (
     <div className="container mx-auto max-w-7xl flex flex-wrap justify-center gap-3 pt-5">
       {loading && <p className="text-center font-bold font-xl">loading...</p>}
