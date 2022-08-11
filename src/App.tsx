2d11560b30fedb6ace44f70ce8546c99f1630955
Prizzz <rob_243248@mail.ru>
2022-08-11 19:44:19 +0400

    feat: add error catching

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import axios from "axios";
+import axios, { Axios, AxiosError } from "axios";
 import React, { useEffect, useState } from "react";
 import Product from "./components/Product";
 import { IProduct } from "./models";
 
 function App() {
   const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   async function fetchProducts() {
-    const res = await axios.get<IProduct[]>(
-      "https://fakestoreapi.com/products?limit=8"
-    );
-    setProducts(res.data);
+    try {
+      setError("");
+      setLoading(true);
+      const res = await axios.get<IProduct[]>(
+        "https://fakestoreapi.com/products?limit=8"
+      );
+      setProducts(res.data);
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as AxiosError;
+      setLoading(false);
+      setError(error.message);
+    }
   }
 
   useEffect(() => {
@@ -19,6 +30,8 @@ function App() {
 
   return (
     <div className="container mx-auto max-w-7xl flex flex-wrap justify-center gap-3 pt-5">
+      {loading && <p className="text-center font-bold font-xl">loading...</p>}
+      {error && <p className="text-center font-bold font-xl">{error}</p>}
       {products.map((product) => (
         <Product product={product} key={product.id} />
       ))}
