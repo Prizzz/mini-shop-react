b2903bc1ad5a5b3e0f0ebcd2a230aea9897a0363
Prizzz <rob_243248@mail.ru>
2022-08-13 01:19:19 +0400

    deleted files

--- src/components/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-interface ErrorMessageProps {
-  error: string;
-}
-
-const ErrorMessage = ({ error }: ErrorMessageProps) => {
-  return <p className="text-center font-bold font-xl">{error}</p>;
-};
-
-export default ErrorMessage;
--- src/components/Loader.tsx
+++
@@ -1,5 +0,0 @@
-const Loader = () => {
-  return <p className="text-center font-bold font-xl">loading...</p>;
-};
-
-export default Loader;
